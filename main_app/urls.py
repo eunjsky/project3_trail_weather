d180799ca9b05e1a30908cbc4402b482382f8d99
Eunyoung Jang <eunj_sky@hotmail.com>
2021-11-03 18:42:51 -0400

    updated-country moderl and weather

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('about/', views.about, name='about'),
     path('weathers/', views.weathers_index, name='index'),
-    path('weathers/<int:city_id>/', views.weathers_detail, name='detail'),
+    path('weathers/<int:city_id>/', views.weathers_detail, name='details'),
     path('trails/', views.trails_index, name='index'),
     path('trails/<int:trail_id>/', views.trails_detail, name='detail'),
     path('trails/create/', views.TrailCreate.as_view(), name='trails_create'),
@@ -13,8 +13,10 @@ urlpatterns = [
          views.TrailUpdate.as_view(), name='trails_update'),
     path('trails/<int:pk>/delete/',
          views.TrailDelete.as_view(), name='trails_delete'),
-    path('trails/<int:trail_id>/assoc_activity/<int:activity_id>/',views.assoc_activity, name='assoc_activity'),
-    path('trails/<int:trail_id>/unassoc_activity/<int:activity_id>/',views.unassoc_activity, name='unassoc_activity'),
+    path('trails/<int:trail_id>/assoc_activity/<int:activity_id>/',
+         views.assoc_activity, name='assoc_activity'),
+    path('trails/<int:trail_id>/unassoc_activity/<int:activity_id>/',
+         views.unassoc_activity, name='unassoc_activity'),
     path('accounts/signup/', views.signup, name='signup'),
 
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -11,7 +11,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 class TrailUpdate(UpdateView):
     model = Trail
-    fields = ['name', 'location', 'description', 'length']
+    fields = ['name', 'location', 'country', 'description', 'length']
 
 
 class TrailDelete(DeleteView):
@@ -21,7 +21,7 @@ class TrailDelete(DeleteView):
 
 class TrailCreate(LoginRequiredMixin, CreateView):
     model = Trail
-    fields = ['name', 'location', 'description', 'length']
+    fields = ['name', 'location', 'country', 'description', 'length']
     success_url = '/trails/'
 
     def form_valid(self, form):
@@ -44,24 +44,19 @@ def trails_detail(request, trail_id):
     activities_trail_doesnt_have = Activity.objects.exclude(
         id__in=trail.activities.all().values_list('id'))
 
-    url = "https://api.openweathermap.org/data/2.5/find?q={}&units=metric&appid=74e08e29a06113b27d6bba189d6e2c27"
+    url = "https://api.openweathermap.org/data/2.5/weather?q={},{}&appid=74e08e29a06113b27d6bba189d6e2c27"
 
-    print(trail.location)
-    r = requests.get(url.format(trail.location))
-    # c = list(filter(lambda item : item['sys']['country'] == 'CA', r['list']))[0]
-    data = json.loads(r)
+    r = requests.get(url.format(trail.location, trail.country)).json()
 
     city_weather = {
-        'city': data['name'],
-        'temperature': data['main']['temp'],
-        'description': data['weather'][0]['description'],
-        'icon': data['weather'][0]['icon'],
-        'speed': data['wind']['speed'],
+        'city': r['name'],
+        'temperature': r['main']['temp'],
+        'description': r['weather'][0]['description'],
+        'icon': r['weather'][0]['icon'],
+        'speed': r['wind']['speed'],
     }
 
-    context = {'city_weather': city_weather}
-    # context = { 'all_weather': r['list'] }
-    return render(request, 'trails/detail.html', {'trail': trail, 'activities': activities_trail_doesnt_have}, context)
+    return render(request, 'trails/detail.html', {'city_weather': city_weather, 'trail': trail, 'activities': activities_trail_doesnt_have})
 
 
 def assoc_activity(request, trail_id, activity_id):
@@ -105,7 +100,7 @@ def weathers_detail(request, city_id):
     }
 
     context = {'city_weather': city_weather}
-    return render(request, 'weathers/detail.html', context)
+    return render(request, 'weathers/details.html', context)
 
 
 def signup(request):
