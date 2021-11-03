a48b952ac1bb7d7bf98fa71282617830ba17235d
지니코 <jinico@Jinny-MacBook-Pro.local>
2021-11-03 01:09:11 -0400

    second commit

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.db.models.fields import CharField
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 # Create your models here.
 
